1f536c05a7af49fe235f1a2dc5aa774c94f52a2d
Alfeen1000 <mcalpineally@gmail.com>
2026-07-24 12:01:24 +0800

    Update lead-capture.js

--- js/lead-capture.js
+++ js/lead-capture.js
@@ -24,7 +24,7 @@
   "use strict";
 
   /* ---- SWAP THIS ------------------------------------------------------ */
-  var GHL_WEBHOOK_URL = "PASTE_YOUR_GHL_INBOUND_WEBHOOK_URL_HERE";
+  var GHL_WEBHOOK_URL = "https://services.leadconnectorhq.com/hooks/xsyaGwI7Qtc8BigBxu9E/webhook-trigger/0ea147c1-3687-40df-b886-a97f9cd6bc83";
   /* --------------------------------------------------------------------- */
 
   var THANK_YOU_URL = "/terima-kasih";
